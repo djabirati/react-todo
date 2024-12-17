56d80cb006a9c619077ea54c5e592b4ff9596d59
ZuurRouani <122629847+ZuurRouani@users.noreply.github.com>
2024-12-17 10:31:11 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Header } from './component/Header';
+import { Footer } from './component/Footer';
+
+// Un composant React
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
+
