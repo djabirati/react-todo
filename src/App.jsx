145a0b5e5115f4a152ec5486e4b7dc128c3cb47a
Djabirati Marthadi <djabirati@MacBook-Pro-de-Djabirati.local>
2024-12-17 15:21:01 +0100

    Exercice ajout de composant Player

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import {Header} from "./components/containers/Header/Header.jsx";
 import {Footer} from "./components/containers/Footer/Footer.jsx";
 import ButtonSubmit from "./components/ui/Button/ButtonSubmit.jsx";
 import {User} from "./components/containers/User/User.jsx";
-
+import './index.css';
+import {Player} from "./components/containers/Player/Player.jsx";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
             <Header />
              <ButtonSubmit />
              <User/>
+             <Player />
             <Footer />
          </div>
 
